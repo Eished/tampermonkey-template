10e71068332fc1bcd5cf290d66df68a635885798
eished <kished@outlook.com>
2022-07-31 17:17:28 +0800

    fix: 点击事件重复触发保存，全局设置不覆盖局部设置，rid类型推导问题

--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,18 @@
-const app = () => {
-  const videoSub = document.querySelector('.layout-Player-videoSub');
-  if (!videoSub) {
-    return;
-  }
-  let rid = new URLSearchParams(window.location.search).get('rid');
-  if (!rid) {
-    const results = window.location.pathname.match(/[\d]{1,10}/);
-    if (results) {
-      rid = results[0];
-    } else {
-      console.debug('斗鱼直播助手：未找到直播间');
-      return;
-    }
-  }
-
+const app = (rid: string, videoSub: Element) => {
   const Clarities = ['全局默认最高画质', '全局默认最低画质'];
   const selectedClarity: string | undefined = GM_getValue(rid);
   const defaultClarity: number | undefined = GM_getValue('defaultClarity');
 
-  const clickClarity = (li: HTMLLIElement) => {
+  const clickClarity = (li: HTMLLIElement, save = false) => {
+    // 阻止点击事件循环
     if (!li.className.includes('selected')) {
+      save ? GM_setValue(rid, li.innerText) : '';
       li.click();
     }
   };
 
-  const clickLi = (li: HTMLLIElement, availableClarity: string) => {
-    GM_setValue(rid!, availableClarity);
-    clickClarity(li);
-  };
-
   const selectClarity = (list: NodeListOf<HTMLLIElement>) => {
-    let notFoundCount = 0;
-    list.forEach((li) => {
-      const availableClarity = li.innerText;
-      if (!availableClarity) {
-        return;
-      }
-      if (selectedClarity === availableClarity) {
-        clickClarity(li);
-      } else {
-        notFoundCount++;
-      }
-      li.addEventListener('click', () => clickLi(li, availableClarity));
-      GM_registerMenuCommand(availableClarity, () => clickLi(li, availableClarity));
-    });
-
-    if (selectedClarity === Clarities[0]) {
-      clickClarity(list[0]);
-    } else if (selectedClarity === Clarities[1]) {
-      clickClarity(list[list.length - 1]);
-    } else if (notFoundCount === list.length) {
-      if (defaultClarity === 0) {
-        clickClarity(list[list.length - 1]);
-      } else {
-        clickClarity(list[0]);
-      }
-    }
-
+    // 注册菜单栏
     Clarities.forEach((clarity, index) => {
       GM_registerMenuCommand(clarity, () => {
         if (index === 0) {
@@ -68,9 +24,37 @@ const app = () => {
         }
       });
     });
+
+    let notFoundCount = 0;
+    list.forEach((li) => {
+      const availableClarity = li.innerText;
+      if (selectedClarity === availableClarity) {
+        // 选择自定义画质
+        clickClarity(li);
+      } else {
+        notFoundCount++;
+      }
+      // 防止误触发保存，仅保存真实点击
+      li.addEventListener('click', (e) => clickClarity(li, e.isTrusted));
+      // 注册菜单栏
+      GM_registerMenuCommand(availableClarity, () => clickClarity(li, true));
+    });
+
+    // 选择默认画质
+    if (selectedClarity === Clarities[0]) {
+      clickClarity(list[0]);
+    } else if (selectedClarity === Clarities[1]) {
+      clickClarity(list[list.length - 1]);
+    } else if (notFoundCount === list.length) {
+      if (defaultClarity === 0) {
+        clickClarity(list[list.length - 1]);
+      } else {
+        clickClarity(list[0]);
+      }
+    }
   };
 
-  const observer = new MutationObserver(() => {
+  const callback = (mutations: MutationRecord[], observer: MutationObserver) => {
     const controller = videoSub?.querySelector(`[value="画质 "]`);
     if (controller) {
       observer.disconnect();
@@ -78,7 +62,9 @@ const app = () => {
       const list = ul?.querySelectorAll('li');
       list ? selectClarity(list) : console.debug('斗鱼直播助手：未找到画质选项');
     }
-  });
+  };
+
+  const observer = new MutationObserver(callback);
 
   observer.observe(videoSub, {
     childList: true,
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 import app from './app';
-app();
+
+let rid = new URLSearchParams(window.location.search).get('rid');
+if (!rid) {
+  const results = window.location.pathname.match(/[\d]{1,10}/);
+  if (results) {
+    rid = results[0];
+  } else {
+    throw new Error('斗鱼直播助手：未找到直播间id');
+  }
+}
+const videoSub = document.querySelector('.layout-Player-videoSub');
+
+if (rid && videoSub) {
+  app(rid, videoSub);
+}
